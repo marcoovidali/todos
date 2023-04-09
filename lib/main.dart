d3ee10064124f89ca7ae067b9a6984665c8c6e38
marcoovidali <marcovidali@email.com>
2023-04-09 23:03:54 +0200

    Removed provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 import 'package:todos/screens/add_a_todo_screen.dart';
 import 'utils/firebase_util.dart';
 
@@ -14,20 +13,15 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => AppState(),
-      child: MaterialApp(
-        title: 'todos',
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
-          fontFamily: 'Quicksand',
-          useMaterial3: true,
-        ),
-        debugShowCheckedModeBanner: false,
-        home: const AddATodoScreen(),
+    return MaterialApp(
+      title: 'todos',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+        fontFamily: 'Quicksand',
+        useMaterial3: true,
       ),
+      debugShowCheckedModeBanner: false,
+      home: const AddATodoScreen(),
     );
   }
 }
-
-class AppState extends ChangeNotifier {}
